ada7802712abe1242c5b3d8cf48fe4e82bb5f641
Hampus Nordin <nordin.hampus@gmail.com>
2015-08-25 08:54:11 +0200

    Removed dev variables from main script and added ENV vars instead

--- web.rb
+++ web.rb
@@ -3,11 +3,13 @@ require "instagram"
 
 enable :sessions
 
-CALLBACK_URL = "http://localhost:4567/oauth/callback"
+INSTAGRAM_CALLBACK_URL = ENV['INSTAGRAM_CALLBACK_URL']
+INSTAGRAM_CLIENT_ID = ENV['INSTAGRAM_CLIENT_ID']
+INSTAGRAM_CLIENT_SECRET = ENV['INSTAGRAM_CLIENT_SECRET']
 
 Instagram.configure do |config|
-  config.client_id = "2159a7b024f148ee9fca5ebd7cbb94ec"
-  config.client_secret = "a4d185fc007b433e839d63e8f46d28cb"
+  config.client_id = INSTAGRAM_CLIENT_ID
+  config.client_secret = INSTAGRAM_CLIENT_SECRET
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
@@ -53,11 +55,11 @@ get "/login" do
 end
 
 get "/oauth/connect" do
-  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+  redirect Instagram.authorize_url(:redirect_uri => INSTAGRAM_CALLBACK_URL)
 end
 
 get "/oauth/callback" do
-  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  response = Instagram.get_access_token(params[:code], :redirect_uri => INSTAGRAM_CALLBACK_URL)
   session[:access_token] = response.access_token
   redirect "/"
 end
