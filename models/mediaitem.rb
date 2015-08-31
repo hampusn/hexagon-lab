8d5387472cc30329b8d510666038ee145d56d038
Hampus Nordin <nordin.hampus@gmail.com>
2015-08-31 07:35:16 +0200

    Corrected validation of instagram id

--- models/mediaitem.rb
+++ models/mediaitem.rb
@@ -1,10 +1,11 @@
 module Hampusn
-	module HexagonLab
-		module Models
+  module HexagonLab
+    module Models
 
-			class MediaItem < ActiveRecord::Base
-			end
-			
-		end
-	end
+      class MediaItem < ActiveRecord::Base
+        validates :item_id, uniqueness: true
+      end
+
+    end
+  end
 end
